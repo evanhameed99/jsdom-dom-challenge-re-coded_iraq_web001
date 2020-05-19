387bf910b7995e03cc1fa6aef963b253c85035e5
Evan hameed <evanhameed9966@gmail.com>
2020-05-19 23:01:22 +0000

    Automatically backed up by Learn

---
+++ js/challenge.js
@@ -0,0 +1,5 @@
+let counter = document.getElementById('counter');
+let minusBtn=document.getElementById('minus');
+let plusBtn=document.getElementById('plus');
+let heartBtn=document.getElementById('heart');
+let pauseBtn=document.getElementById('pasue');
